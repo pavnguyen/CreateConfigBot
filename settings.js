f11c6fb836374a3f7e4cb5dca35f0800d090d4c6
pavnguyen <vunguyen.xbt@gmail.com>
2018-04-27 22:43:18 +0700

    ok

---
+++ settings.js
@@ -0,0 +1,16 @@
+{
+    "binance": {
+        "first_position": 1,
+        "last_position": 20,
+        "number_bot":5,
+        "type":"BTC",
+        "strategies": "custom"
+    },
+    "bittrex": {
+        "first_position": 1,
+        "last_position": 20,
+        "number_bot":5,
+        "type":"BTC",
+        "strategies": "custom"
+    } 
+}
